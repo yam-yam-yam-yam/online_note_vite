f861eb2854847d6b0a1c4a5194b0f5ed6392b0f3
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-07-23 21:26:13 +0900

    [feat] 로그인 화면

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
 
+import { storeUser } from "./store/user";
+
+const checkLogin = () => {
+  return storeUser().getUserId;
+}
+
+if(!checkLogin()) {
+  window.location.href='#/login';
+}
+
 </script>
 
 <template>
-
+  <router-view></router-view>
 </template>
---
+++ src/page/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { ref } from "vue";
+import { storeUser } from "../store/user";
+
+let userId = ref(undefined);
+
+const login = (param) => {
+    storeUser().setUserId(param)
+    alert(`${storeUser().getUserId} 계정으로 접속합니다.`);
+    window.location.href='#/memo';
+}
+
+</script>
+
+<template>
+    <input type="text" v-model="userId">아이디 입력
+    <button @click="login(userId)">로그인</button>
+</template>
